823d542c0c765236a485af259432f35b07d72191
Jack Sims <123803257+sym-z@users.noreply.github.com>
2024-12-05 20:54:44 -0800

    Base Capacitor working with Xcode

---
+++ android/app/src/main/java/team2/final/app/MainActivity.java
@@ -0,0 +1,5 @@
+package team2.final.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli'
+
+const config: CapacitorConfig = {
+    appId: 'team2.final.app',
+    appName: '121-final-team-2',
+    webDir: './dist/',
+}
+
+export default config
